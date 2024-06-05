5db5ecf16f176642fb57069ae92d78c60672635d
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-06-06 00:55:04 +0900

    add: 2章クリア

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -51,6 +52,11 @@ func realMain() error {
 	mux := router.NewRouter(todoDB)
 
 	// TODO: サーバーをlistenする
+	log.Println("Starting server on port", port)
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
